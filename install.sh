5f16baf59f9dcbac48604ed7afbca4ba23030b05
Bill DeCoste <wdecoste@redhat.com>
2016-11-14 10:49:58 -0800

    update

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 set -x
 
 source /usr/local/s2i/install-common.sh
+source /usr/local/s2i/install-teiid-common.sh
 
 injected_dir=$1
 
 install_modules ${injected_dir}/injected-modules
+
+configure_translators ${injected_dir}/install.properties
