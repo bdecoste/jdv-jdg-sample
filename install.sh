59e1ca905ce6ce3ba53a435f4b194e2fb4c8eff9
Bill DeCoste <wdecoste@redhat.com>
2016-11-14 12:58:52 -0800

    update

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ injected_dir=$1
 
 install_modules ${injected_dir}/injected-modules
 
-configure_translators ${injected_dir}/install.properties
+#configure_translators ${injected_dir}/install.properties
