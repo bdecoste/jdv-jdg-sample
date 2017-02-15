1f960a985f7ca8757fe6bf3500baa4e97e9aaf3e
Bill DeCoste <wdecoste@redhat.com>
2017-02-15 08:08:21 -0800

    update

--- injected-files/example.sql
+++ injected-files/example.sql
@@ -1,3 +1,19 @@
+CREATE TABLE status
+(
+  VDBName varchar(50) not null,
+  VDBVersion integer not null,
+  SchemaName varchar(50) not null,
+  Name varchar(256) not null,
+  TargetSchemaName varchar(50),
+  TargetName varchar(256) not null,
+  Valid boolean not null,
+  LoadState varchar(25) not null,
+  Cardinality bigint,
+  Updated timestamp not null,
+  LoadNumber bigint not null,
+  PRIMARY KEY (VDBName, VDBVersion, SchemaName, Name)
+);
+
 CREATE TABLE mytable
 (
    _id varchar(64),
@@ -5,6 +21,6 @@ CREATE TABLE mytable
    CONSTRAINT MYTABLE_PK PRIMARY KEY(_id)
 );
 
-INSERT INTO mytable (_id,_value) ('000001','Bill');
+INSERT INTO mytable (_id,_value) VALUES ('000001','Bill');
 
 
